7f12a0be0b59589daae101649d2afccdcaccf0d5
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-25 15:49:40 -0300

    feat/wip: company service

---
+++ src/company/company.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CompanyService {
+    findAll(){
+
+    }
+
+    findOne(id: string){
+        
+    }
+
+    create(body: any){
+        
+    }
+
+    update(param: any, body: any, header: any){
+        
+    }
+}
