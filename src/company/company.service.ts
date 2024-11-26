ca0737bf8fcc781b4fb5434f6c773c9a4a098918
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-26 16:40:07 -0300

    feat: create company service crud logic

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,20 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Company } from 'src/typeOrm/Company'; // Certifique-se de que o caminho está correto
+import { CreateCompanyDto } from './dto/create-company.dto';
+import { UpdateCompanyDto } from './dto/update-company';
 
 @Injectable()
 export class CompanyService {
-    findAll(){
+  constructor(
+    @InjectRepository(Company)
+    private companyRepository: Repository<Company>,
+  ) {}
 
-    }
+  findAll(): Promise<Company[]> {
+    return this.companyRepository.find();
+  }
 
-    findOne(id: string){
-        
-    }
+  findOne(id: number): Promise<Company> {
+    return this.companyRepository.findOne({where: {id}});
+  }
 
-    create(body: any){
-        
-    }
+   async create(body: CreateCompanyDto): Promise<Company> {
+    const company = this.companyRepository.create(body);
+    return this.companyRepository.save(company);
+  }
 
-    update(param: any, body: any, header: any){
-        
-    }
+  async update(id: number, body: UpdateCompanyDto): Promise<Company> {
+    await this.companyRepository.update(id, body);
+    return this.companyRepository.findOne({ where: { id } });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.companyRepository.delete(id);
+  }
 }
