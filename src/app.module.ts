c6c422da833c94be156a2e2b8e8eded5cc0f5177
Jamilson Jr <jamilsonjr@live.com>
2024-11-25 22:46:32 -0300

    feat: adjust app to connect w/ db, access env file and use entities array

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CompanyModule } from './company/company.module';
+import entities from './typeOrm';
 
 @Module({
-  imports: [CompanyModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+        entities: Object.values(entities),
+      }),
+    }),
+    CompanyModule,
+  ],
 })
 export class AppModule {}
