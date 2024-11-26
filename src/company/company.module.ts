5d9358777f23cd0b9d0a41275c511792acf4776e
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-26 16:40:59 -0300

    feat: add typeorm acces by company module

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -2,9 +2,12 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { CompanyController } from './company.controller';
 import { APP_PIPE } from '@nestjs/core';
 import { CompanyService } from './company.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Company } from 'src/typeOrm/Company';
 
 @Module({
   controllers: [CompanyController],
+  imports: [TypeOrmModule.forFeature([Company])],
   providers : [{
     provide: APP_PIPE,
     useValue: new ValidationPipe({
