28d29558e923132bb3930a6cabe798c7873b0687
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-26 16:40:40 -0300

    feat: adjust company controller ingration with service

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,40 +1,47 @@
-import { Body, Controller, Get, Headers, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { CreateCompanyDto } from './dto/createCompany.dto';
+import { Body, Controller, Delete, Get, Headers, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CreateCompanyDto } from './dto/create-company.dto';
 import { IdParamDto } from './dto/idParam.dto';
 import { HeadersDto } from './dto/headers.dto';
 import { RequestHeader } from 'src/request-header';
 import { CompanyService } from './company.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Company } from 'src/typeOrm/Company';
+import { Repository } from 'typeorm';
 
 @Controller('company')
 export class CompanyController {
     companyService: CompanyService;
 
-    constructor(companyService: CompanyService){
-        this.companyService = companyService;
-    }
+    constructor(
+        @InjectRepository(Company)
+        private companyRepository: Repository<Company>,
+      ) {}
     
-    @Get()
-    findAll(){
-        return this.companyService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.companyService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string){
-        return this.companyService.findOne(id);
-    }
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.companyService.findOne(id);
+  }
 
-    @Post()
-    @UsePipes(new ValidationPipe({whitelist: true, forbidNonWhitelisted: true, always: true}))
-    create(@Body() body: CreateCompanyDto){
-        return this.companyService.create(body);
-    }
+  @Post()
+  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+  create(@Body() createCompanyDto: CreateCompanyDto) {
+    return this.companyService.create(createCompanyDto);
+  }
 
-    @Patch(':id')
-    update(@Param() param, 
-            @Body() body: CreateCompanyDto,
-            @RequestHeader(new ValidationPipe({validateCustomDecorators: true})) header: HeadersDto,
-    ){
-        return this.companyService.update(param, body, header);
-    }
+  @Patch(':id')
+  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+  update(@Param('id') id: number, @Body() updateCompanyDto: CreateCompanyDto) {
+    return this.companyService.update(id, updateCompanyDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.companyService.remove(id);
+  }
 
 }
