c1dbe055114671e28aafd5869dc78b1e649f32f4
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-26 16:39:30 -0300

    fix: rename file to maintain application pattern

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,12 +1,11 @@
-import { IsString, Length } from "class-validator";
+import { IsInt, IsPositive, IsString, Length } from "class-validator";
 
 export class CreateCompanyDto {
-    @IsString({always: true})
-    readonly id: string;
+    @IsInt({always: true})
+    @IsPositive()
+    readonly id: number;
     @IsString()
     readonly name: string;
-    @IsString()
-    readonly address: string;
     @IsString()
     @Length(8, 40, {message: "Phone number must be between 8 and 40 characters"})
     readonly number: string;
