9bf57f4a78b1f5c73b68cad26bc70d82454934be
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-26 16:41:42 -0300

    feat: create and update user dtos

---
+++ src/user/dto/update-user.ts
@@ -0,0 +1,18 @@
+import { IsString, IsNotEmpty, IsEmail, IsOptional } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  username?: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  password?: string;
+}
