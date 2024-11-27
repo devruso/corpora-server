b6fe052ee9db299dfb9ce2a7a7c791cc28e85b21
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-27 11:20:23 -0300

    feat: create auth controller login and login via google functions

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, HttpCode, HttpStatus, Post, Req, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { GoogleAuthGuard } from './guard/google-auth/google-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(@Request() req){
+    const token = this.authService.login(req.user.id)
+    return {id: req.user.id, token}
+  }
+
+
+  @UseGuards(GoogleAuthGuard)
+  @Get('google/login')
+  googleLogin(){
+
+  }
+
+  @UseGuards(GoogleAuthGuard)
+    @Get('google/callback')
+    googleCallback(@Req() req){
+      //const response = await this.authService.login(req.user.id)
+    }
+  
+}
