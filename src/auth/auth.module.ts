af5b499146eb93dad0bc5b7191a785199f49748b
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-27 13:22:50 -0300

    feat: add jwt strategy on auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,14 +11,16 @@ import jwtConfig from './jwt/jwt.config';
 import { UserModule } from 'src/user/user.module';
 import { UserService } from 'src/user/user.service';
 import { LocalStrategy } from 'src/strategies/local.strategy';
+import { JwtStrategy } from 'src/strategies/jwt.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]),
   JwtModule.registerAsync(jwtConfig.asProvider()),
   ConfigModule.forFeature(googleOauthConfig),
+  ConfigModule.forFeature(jwtConfig),
   UserModule
 ],
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy, UserService, LocalStrategy],
+  providers: [AuthService, GoogleStrategy, UserService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
