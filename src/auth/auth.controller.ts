50cf8a36915a2eb2ca66fc9593240043d697f166
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-27 23:32:03 -0300

    feat: add guards and adjust function calling on auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, HttpCode, HttpStatus, Post, Req, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Post, Req, Request, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { GoogleAuthGuard } from './guard/google-auth/google-auth.guard';
 import { AuthGuard } from '@nestjs/passport';
+import { RefreshAuthGuard } from './guard/refresh-auth/refresh-auth.guard';
+import { JwtAuthGuard } from './guard/jwt-auth/jwt-auth.guard';
+import { Roles } from './decorators/role.decorator';
+import { Role } from './enums/role.enum';
+import { RolesGuard } from './guard/roles/roles.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -12,10 +17,28 @@ export class AuthController {
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Request() req){
-    const token = this.authService.login(req.user.id)
-    return {id: req.user.id, token}
+    return this.authService.login(req.user.id);
   }
 
+  @UseGuards(RefreshAuthGuard)
+  @Post('refresh')
+  refreshToken(@Request() req){
+    return this.authService.refreshToken(req.user.id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('signout')
+  async signOut(@Request() req) {
+    return this.authService.signOut(req.user.id);
+  }
+
+  @Roles(Role.ADMIN)
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @Get('users/all')
+  async getAllUsers(){
+    return this.authService.getAllUsers();
+  }
 
   @UseGuards(GoogleAuthGuard)
   @Get('google/login')
@@ -25,8 +48,9 @@ export class AuthController {
 
   @UseGuards(GoogleAuthGuard)
     @Get('google/callback')
-    googleCallback(@Req() req){
-      //const response = await this.authService.login(req.user.id)
+    async googleCallback(@Req() req, @Res() res){
+      const response = await this.authService.login(req.user.id);
+      res.redirect(`http://localhost:3001?token=${response.accessToken}&refreshToken=${response.refreshToken}`);
     }
   
 }
