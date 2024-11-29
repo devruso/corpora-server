149bf41d06d698e48d50285e71a0735da222c1b0
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-29 19:40:43 -0300

    fix: adjust update company dto

--- src/company/dto/update-company.ts
+++ src/company/dto/update-company.ts
@@ -14,4 +14,14 @@ export class UpdateCompanyDto {
   @Length(8, 40, { message: 'Phone number must be between 8 and 40 characters' })
   @IsOptional()
   readonly number?: string;
+
+  @IsString()
+  @Length(8, 40, {message: "O número de telefone deve ter entre 8 e 40 caracteres"})
+  @IsOptional()
+  readonly phoneNumber?: string;
+
+  @IsString()
+  @Length(14, 14, {message: "O CNPJ deve ter 14 caracteres"})
+  @IsOptional()
+  readonly cnpj?: string;
 }
