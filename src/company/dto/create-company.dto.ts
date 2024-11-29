88f386b39cf74d52a3db5082ce1fa04d00145e30
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-29 19:40:31 -0300

    fix: adjust create company dto

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -4,7 +4,10 @@ export class CreateCompanyDto {
     @IsString()
     readonly name: string;
     @IsString()
-    @Length(8, 40, {message: "Phone number must be between 8 and 40 characters"})
-    readonly number: string;
+    @Length(8, 40, {message: "O número de telefone deve ter entre 8 e 40 caracteres"})
+    readonly phoneNumber: string;
+    @IsString()
+    @Length(14, 14, {message: "O CNPJ deve ter 14 caracteres"})
+    readonly cnpj: string;
 
 }
